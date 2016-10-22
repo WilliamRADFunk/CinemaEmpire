c03518971f65f4f4343f679b0e455dfd6f02f824
William RAD Funk <contact@WILLIAMRADFunk.com>
2016-10-22 01:25:13 -0400

    Finished a single pass along the whole pipeline to get a full detailed list of movies in the db.

---
+++ CinemaEmpire.Data/Entities/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaEmpire.Data.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public float ExpectedPopularity { get; set; }
+        public float ActualPopularity { get; set; }
+        public int OptimalSeason { get; set; }
+        public int WorstSeason { get; set; }
+        public float CostLicense { get; set; }
+        public int LicenseLength { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(int id, string title, string synopsis, float expectedPopularity, float actualPopularity, int optimalSeason, int worstSeason, float costLicense, int licenseLength)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Synopsis = synopsis;
+            this.ExpectedPopularity = expectedPopularity;
+            this.ActualPopularity = actualPopularity;
+            this.OptimalSeason = optimalSeason;
+            this.WorstSeason = worstSeason;
+            this.CostLicense = costLicense;
+            this.LicenseLength = licenseLength;
+        }
+    }
+}
---
+++ CinemaEmpire.Data/Repositories/CinemaRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaEmpire.Data.Entities;
+using MySql.Data.MySqlClient;
+
+namespace CinemaEmpire.Data.Repositories
+{
+    public class CinemaRepository
+    {
+        /// <summary> Gets all of the movies in the Movie table and returns them in list form </summary>
+        public List<Movie> GetAvailableMovies()
+        {
+            List<Movie> Movies = new List<Movie>();
+
+            MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = "server=mysql.creativecrew.org;uid=williamfunk;pwd=williamchang;database=creativecrew_cinemaempire;";
+
+            try
+            {
+                using (conn = new MySqlConnection())
+                {
+                    conn.ConnectionString = myConnectionString;
+                    conn.Open();
+
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "SELECT * FROM Movie";
+
+                        MySqlDataReader data = cmd.ExecuteReader();
+                        while (data.Read())
+                        {
+                            Movie movie = new Movie()
+                            {
+                                Id = (int)data["Id"],
+                                Title = (string)data["Title"],
+                                Synopsis = (string)data["Synopsis"],
+                                ExpectedPopularity = (float)data["ExpectedPopularity"],
+                                ActualPopularity = (float)data["ActualPopularity"],
+                                OptimalSeason = (int)data["OptimalSeason"],
+                                WorstSeason = (int)data["WorstSeason"],
+                                CostLicense = (float)data["CostLicense"],
+                                LicenseLength = (int)data["LicenseLength"]
+                            };
+                            Movies.Add(movie);
+                        };
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return Movies;
+        }
+    }
+}
